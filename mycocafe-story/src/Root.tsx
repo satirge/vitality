db9916ded19e4436fbbae6160b859614f95fb3f8
satirge <satir.ge@gmail.com>
2026-04-15 14:22:38 +0200

    Create Root.tsx for Remotion project

---
+++ mycocafe-story/src/Root.tsx
@@ -0,0 +1,15 @@
+import { Composition } from "remotion";
+import { MycoCafeStory } from "./MycoCafeStory";
+
+export const RemotionRoot: React.FC = () => {
+  return (
+    <Composition
+      id="MycoCafeStory"
+      component={MycoCafeStory}
+      durationInFrames={450}
+      fps={30}
+      width={1080}
+      height={1920}
+    />
+  );
+};
